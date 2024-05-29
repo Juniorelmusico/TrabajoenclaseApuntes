6239b27a194a181a11c5d323b485d87fc804b9fe
laboratorio <laboratorio@UPOAULA10611>
2024-05-29 09:01:58 -0500

    Agregar archivos de proyecto.

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+namespace Notes;
+
+public partial class AboutPage : ContentPage
+{
+
+
+      private async void LearnMore_Clicked(object sender, EventArgs e)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync("https://aka.ms/maui");
+        }
+    
+}
