f8823be08e8e3f0f612cd98f153799aa2be39587
Junior Humberto Espin Escobar <junior.espin@udla.edu.ec>
2024-05-30 23:00:25 -0500

    Ultimo Codigo

---
+++ Views/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+namespace Notes.Views;
+
+public partial class AboutPage : ContentPage
+{
+
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+
+}
+
